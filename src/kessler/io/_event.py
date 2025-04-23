37eeac0c3cb628d061dbcc68c9bb13993b7d75ce
Boniface Yogendran <yogi@Bonifaces-MacBook-Pro.local>
2025-04-23 09:38:46 -0400

    WIP event init

--- src/kessler/io/_event.py
+++ src/kessler/io/_event.py
@@ -1,8 +1,30 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+import os
+from ._CDM import CDM
 
 
 class Event(BaseModel):
-    cdms: list
+    cdms: list | str
+
+    @field_validator("cdms")
+    def validate_cdms(cls, cdms):
+        if isinstance(cdms, str):
+            if os.path.exists(cdms):
+                files = os.listdir(cdms)
+                file_names = [os.path.join(cdms, file) for file in files if file.endswith(".yaml")]
+
+                cdms = [CDM.load(file_name=file_name) for file_name in file_names]
+            else:
+                raise ValueError(f"File {cdms} does not exist.")
+        else:
+            if all(isinstance(c, CDM) for c in cdms):
+                pass
+            else:
+                raise ValueError(
+                    "cdms must be a list of CDM objects or a string representing a file path."
+                )
+
+        return cdms
 
     def __repr__(self):
         return "Event(CDMs: {})".format(len(self.cdms))
@@ -22,3 +44,6 @@ class EventDataset(BaseModel):
             return "EventDataset(Events:{}, number of CDMs per event: {} (min), {} (max), {:.2f} (mean))".format(
                 len(self.events), event_lengths_min, event_lengths_max, event_lengths_mean
             )
+
+
+CNDOT_NDOT: 0.14935299999999999
