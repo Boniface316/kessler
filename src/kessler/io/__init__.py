2564f083964b0f7a1e1cdc1f22459e81cde146d7
Boniface Yogendran <yogi@Bonifaces-MacBook-Pro.local>
2025-06-18 20:13:37 -0400

    refact adn notebook

--- src/kessler/io/__init__.py
+++ src/kessler/io/__init__.py
@@ -1,7 +1,8 @@
 from .csv import CSVReader, CSVWriter
 from .configs import Config
+from ._CDM import CDM
 
 ReaderKind = CSVReader
 WriterKind = CSVWriter
 
-__all__ = ["Config", "CSVReader", "CSVWriter"]
+__all__ = ["Config", "CSVReader", "CSVWriter", "CDM"]
